a4556d90a9da2556d83a99303176ebb47c0fbe05
snirkhey <snirkhey@gmail.com>
2025-08-20 22:04:43 +0530

    feat: Add C and C++ test files with fixes

---
+++ test.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+	char* c = "Hello from C!";
+	printf("%s",c);
+	return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+int main()
+{
+	const char* c = "Hello from C++!";
+	std::cout<<c;
+	return 0;
+}
